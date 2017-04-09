a07fda37fdec862626733486376b67da09d4abc2
Stefan Bicher <stefan.bicher@data-experts.de>
2017-04-09 06:43:08 +0200

    Moved model-classes to src/model

---
+++ src/model/Race.ts
@@ -0,0 +1,21 @@
+/**
+ * Class representing a single race event, e.g. in the calendar
+ */
+export class Race {
+	public EventID: number; // ID of the event in the DUV-statistics
+	public EventName: string; // name of the event
+	public Edition: string; // number of repetition of the event (e.g. '3.')
+	public City: string; // city, where the event is hold
+	public Country: string; // country, where the event is hold
+	public EventType: number; // enum for the type of the event (numbers from 1-6)
+	public Results: string; // Link to the results
+	public Length: string; // length of the race (in km)
+	public Duration: string; // duration for Fixed-Hour-Races, e.g.  '6h'
+	private IAULabel: string; // enum for the Label of the IAU (International Association of Ultrarunners); possible values: 'N' == 'None', 'B' == 'Bronze'
+	private Startdate: string; // start date in the format 'yyyy-MM-dd'
+	private Enddate: string; // start date in the format 'yyyy-MM-dd HH:mm:ss'
+	private Year: number; // year of the race
+	private Month: number; // month of the race (e.g. 4 == 'April')
+	private Cupname: string; 
+	private CupYear: string; 
+}
--- src/pages/races/races.ts
+++ src/pages/races/races.ts
@@ -1,17 +1,68 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import {CalendarService} from '../../services/CalendarService';
+import {Race} from '../../model/Race';
 
 @Component({
   selector: 'page-races',
   templateUrl: 'races.html',
+  providers: [CalendarService]
 })
 export class RacesPage {
+	private races : Array<Race>;
+	
+	
+  constructor(public navCtrl: NavController, public navParams: NavParams, private calendarService: CalendarService) {
+  	// TODO: 'loading....'-Icon
+  	this.races = JSON.parse(`
+	{
+		"races":[{
+			"EventName": "Rennen 1",
+			"EventDateLocal": "01.01.2014"
+		},
+		{
+			"EventName": "Rennen 2",
+			"EventDateLocal": "02.01.2014"
+		},
+		{
+			"EventName": "Letztes Rennen ",
+			"EventDateLocal": "18.09.2017"
+		}
+		]
+	}
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+		`).races;
+
+  	this.queryCalendar();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad Races');
-  }
+  queryCalendar() {
+        this.calendarService.queryCalendar('GER').subscribe(
+				data => {
+                    console.log("found data: " + data);
 
+                    data.Races.forEach (function(race) {
+						var dateAsString = race.Startdate;
+						if (dateAsString.length < 10) {
+							return;
+						}
+	
+						var dateParts = dateAsString.split("-");
+						var raceDate = new Date(dateParts[0],(dateParts[1]-1), dateParts[2] );
+						
+		//				var diff = raceDate - today;
+		
+						//if (diff > 0 && diff < maxDiffInMillis) {	
+						var raceDateFormatted = dateParts[2] + '.'+ dateParts[1] + '.' + dateParts[0];
+						console.log (raceDateFormatted + ' ' + race.Results);
+						//	}
+					});
+
+                },
+                err => {
+                    console.log(err);
+                },
+                () => console.log('Suche wurde beendet...')
+        	);
+    }
 }
